a7b7a6e0cfb5a942878f890ef027fac4c590cd67
Dakshi <dakshiegoel@gmail.com>
2022-09-07 16:52:05 +0530

    bubble sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -10,8 +10,7 @@ public class BubbleSort {
 	                                 temp = arr[j-1];  
 	                                 arr[j-1] = arr[j];  
 	                                 arr[j] = temp;  
-	                         }  
-	                          
+	                         }
 	                 }  
 	         }  }
 	 public static void main(String[] args) {  
@@ -22,9 +21,7 @@ public class BubbleSort {
                  System.out.print(arr[i] + " ");  
          }  
          System.out.println();  
-           
          bubbleSort(arr);
-          
          System.out.println("Array After Bubble Sort");  
          for(int i=0; i < arr.length; i++){  
                  System.out.print(arr[i] + " ");  
