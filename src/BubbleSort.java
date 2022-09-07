f12740afb6f6833cad4b0007340b7849ddc798b8
Dakshi <dakshiegoel@gmail.com>
2022-09-07 16:34:12 +0530

    bubble sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -7,7 +7,6 @@ public class BubbleSort {
 	         for(int i=0; i < n; i++){  
 	                 for(int j=1; j < (n-i); j++){  
 	                          if(arr[j-1] > arr[j]){  
-
 	                                 temp = arr[j-1];  
 	                                 arr[j-1] = arr[j];  
 	                                 arr[j] = temp;  
